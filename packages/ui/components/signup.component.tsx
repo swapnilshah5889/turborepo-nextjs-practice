a65832693b6e52ca7df2c35d48c2da5a7d1a66d1
Swapnil Shah <swapnilshah5889@gmail.com>
2023-08-19 10:45:23 -0400

    Implemented client specific on click method in signup component

--- packages/ui/components/signup.component.tsx
+++ packages/ui/components/signup.component.tsx
@@ -1,12 +1,30 @@
+import { Typography, Button, TextField } from "@mui/material"
+import { useState } from "react";
 
-export function SignUp() {
-    return <div>
-        <label htmlFor="email">Email :</label>
-        <input type="text" id="email" ></input>
-        <br />
-        <label htmlFor="password">Password :</label>
-        <input type="text" id="password"/>
-        <br />
-        <button type="submit">Submit</button>
-    </div>
+export function SignUp(props) {
+
+    const [username, setUserName] = useState("");
+    const [password, setPassword] = useState("");
+
+
+
+    return (
+        <div>
+            <label htmlFor="email">Email :</label>
+            <input type="text" id="email" onChange={ (event) => {
+                console.log(event.target.value);
+                setUserName(event.target.value);
+            }}></input>
+            <br />
+            <label htmlFor="password">Password :</label>
+            <input type="text" id="password" onChange={ (event) => {
+                console.log(event.target.value);
+                setPassword(event.target.value);
+            }}/>
+            <br />
+            <button type="submit" onClick={async () => {
+                props.onClick(username, password);
+            }}>Submit</button>
+        </div>
+    );
 }
